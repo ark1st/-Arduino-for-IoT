f7b719a7ad244d13f01ff6aa4d0d5556c5a1896a
ark <ark8846@gmail.com>
2020-07-14 14:34:51 +0900

    directory

---
+++ C/0715/if_1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int input = 0;
+    int pw = 1234;
+    printf("비밀번호를 입력하세요 : ");
+    scanf("%d",&input);
+
+    if (pw == input) {
+        printf("비밀번호가 맞았습니다.");
+    }
+    else {
+        printf("비밀번호가 틀렸습니다.");
+    }
+}
+
